c359fd15b5d0457c6f64d0e049ed792bc7d0ae2b
LAPTOP-4EIRUTHI\fearl <fearlessleader2.0@gmail.com>
2018-04-10 00:28:04 -0600

    Just need to impliment union find

--- Social Network/Social Network/TestNetwork.cpp
+++ Social Network/Social Network/TestNetwork.cpp
@@ -2,13 +2,46 @@
 #include "friendGroup.h"
 #include "unionFind.h"
 #include <assert.h>
+#include <iostream>
+#include <string>
 #include <fstream>
+#include <time.h>
 
 
 
+bool isComplete(Friend list[]);
 
 void main()
 {
+	const int listSize = 100;
+	Friend theList [listSize];
+	srand(time(NULL));
+	bool finished = false;
+	int days = 0;
 
+	for(int i = 0; i < listSize; i++)
+	{
+		theList[i].id = i;
+	}
+	
+	while (!finished)
+	{
+		days++;
+		int f1 = rand() % 100 + 1;
+		int f2 = rand() % 100 + 1;
+		if(f1 == f2)
+			f2 = rand() % 100 + 1;
+		theList[f1].connect(f2);
+		if (isComplete(theList))
+			finished = true;
+
+	}
+
+	
+	std::cout << days << std::endl;
+}
+
+bool isComplete(Friend list[])
+{
 
 }
