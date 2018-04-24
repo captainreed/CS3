7508db3feeb5c9c84d44b35f5ecbf2db652f8b04
LAPTOP-4EIRUTHI\fearl <fearlessleader2.0@gmail.com>
2018-04-23 22:54:56 -0600

    Print functions and data structures complete

--- Walk Cleaning/TestEuler.cpp
+++ Walk Cleaning/TestEuler.cpp
@@ -6,17 +6,34 @@
 using namespace std;
 
 void main ()
-{  	ofstream fout;
-	fout.open("tourOut.txt");
-	assert(fout);
+{  
+	//ofstream fout;
+	//fout.open("tourOut.txt");
+	//assert(fout);
 
 
-   Graph g("prog7A.txt",fout);
-   g.computeTour(fout);  // If I want the output to appear on console, I just make the parameter "cout"
+   Graph g("prog7A.txt");
 
-   Graph g1("prog7B.txt",fout);
-   g1.computeTour(fout);
+   cout << "prog 7A edge list " << endl;
+   g.pringEdgeList();
+   cout << "prog 7A Matrix  " << endl;
+   g.printAdjMatrix();
+  // g.computeTour(fout);  // If I want the output to appear on console, I just make the parameter "cout"
 
-   Graph g2("prog7C.txt",cout);
-   g2.computeTour(cout);
+   Graph g1("prog7B.txt");
+   cout << "prog 7B edge list " << endl;
+   g1.pringEdgeList();
+   cout << "prog 7B Matrix  " << endl;
+   g1.printAdjMatrix();
+  // g1.computeTour(fout);
+
+   Graph g2("prog7C.txt");
+   cout << "prog 7C edge list " << endl;
+   g2.pringEdgeList();
+   cout << "prog 7C Matrix  " << endl;
+   g2.printAdjMatrix();
+   //g2.computeTour(cout);
+
+   string wait;
+   cin >> wait;
 }
--- Walk Cleaning/Walk Cleaning/Graph.h
+++ Walk Cleaning/Walk Cleaning/Graph.h
@@ -7,43 +7,75 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <math.h>
 #include "edge1.h"
+
 
 using namespace std;
 
 class Graph
 {
 public:
-	vector <Edge>theGraph;
+	vector <Edge> edgeList;
 	int size;
 	vector<vector<int>> adjmatrix;
 	
-
 	Graph(string filepath) 
 	{
 		ifstream fin(filepath);
 		string line;
-		getline(fin, line);
-		size = (int)line[0];
-		
-	
-		for (int i = size; i < size; i++)
-		{
-			for (int j = size; j < size; j++)
+		if (!fin.is_open()) {
+			cout << "aint no file up in here" << endl;
+			return;
+		}
 
+		getline(fin, line);
+
+		for (int i = 0; line[i] != ' '; i++)
+		{
+			size = ((int)line[0] - '0')*pow(10,i);
 		}
 		
+
+		vector<int> secondary(size, -1);
+		adjmatrix.resize(size, secondary);
 		while (getline(fin, line))
 		{
 			Edge newedge;
-			newedge.set(line[0], line[1]);
-			theGraph.push_back(newedge);
-
+			if (line != "") 
+			{
+				newedge.set(line[0], line[2]);
+				edgeList.push_back(newedge);
+				addEdge(newedge);
+			}
 		}
-
-
-
+		fin.close();
 	};
+
+	void addEdge(Edge e1)
+	{
+		adjmatrix[e1.fromNode][e1.toNode] = 1;
+		adjmatrix[e1.toNode].at(e1.fromNode) = 1;
+	}
+	void printAdjMatrix()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				int out = adjmatrix[i][j];
+				cout << out;
+			}
+			cout << "" << endl;
+		}
+	}
+	void pringEdgeList()
+	{
+		for (int i = 0; i < edgeList.size(); i++)
+		{
+			cout << edgeList[i].toString() << endl; 
+		}
+	}
 
 	void computeTour(ofstream output)
 	{
